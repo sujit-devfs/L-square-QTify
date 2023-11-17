fcdae595e3d810123b677e66ec725bd55c059329
sujit-devfs <sujit05061994@gmail.com>
2023-11-18 04:16:24 +0530

    C4:Search Bar

--- qtify/src/Components/SearchBar/index.js
+++ qtify/src/Components/SearchBar/index.js
@@ -1,5 +1,31 @@
-export default () => {
+import {ReactComponent as SearchIcon} from "./Search icon.svg";
+
+ const Button = () => {
     return(
-        <h1>searchbar</h1>
+        <form style={{
+            display:"flex",
+            minWidth:"568px"
+        }}>
+            <input 
+            placeholder="Search an album of your choice"
+            style={{
+                flex:"1",
+                borderRadius:"8px 0px 0px 8px",
+                padding:"8px"
+            }}
+            />
+            <button style={{
+                width: "66px",
+                height:"48px",
+                display:"flex",
+                justifyContent:"center",
+                alignItems:"center",
+                borderRadius: "0px 8px 8px 0px"
+            }}>
+                <SearchIcon/>
+            </button>
+        </form>
     )
 }
+
+export default  Button;
