6af1c0d8df345e2f2a7e963d86e45f1968ec1e88
sujit-devfs <sujit05061994@gmail.com>
2023-11-25 05:13:25 +0530

    hero-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import HeroSection from './Components/HeroSection';
 import NavBar from './Components/NavBar';
+
 
 function App() {
   return (
     <>
     <NavBar />
+    <HeroSection />
     </> 
   );
 }
---
+++ qtify/src/Components/HeroSection/index.jsx
@@ -0,0 +1,17 @@
+import "./herosection.css";
+
+export default () => {
+    return(
+        <div className="hs-container">
+            <div className="hs-content">
+                <div>
+                    <h1>100 Thousand Songs, ad-free</h1>
+                    <h1>Over thousands podcast episodes</h1>
+                </div>
+                <div className="hs-img-frame">
+                    <img src="vibrating-headphone 1.png" className="hs-img" />
+                </div>
+            </div>
+        </div>
+    )
+}
